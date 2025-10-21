28b9e28af32ade1ab5bd715c1c4cbc7fc2bf6c93
Sirinya <66310044@g0.buu.ac.th>
2025-10-21 10:29:26 +0700

    Add Swagger and ReDoc API Documentation

--- myproject/settings.py
+++ myproject/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework', 
     'students',  
+    'drf_yasg',      
+    'corsheaders',     
 ]
 
 MIDDLEWARE = [
@@ -124,6 +126,7 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
+    'DEFAULT_SCHEMA_CLASS': 'rest_framework.schemas.coreapi.AutoSchema',
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 20
 }
